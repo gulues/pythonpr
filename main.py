56b964d8ae148db84558199a0a482b91dddc40ef
gulues <gulues@gmail.com>
2020-08-05 21:00:54 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import sympy.ntheory as nt 
+
+def countPrimes(primo,anterior):
+    count=0
+    nt
+    prime=int(primo)
+    for i in range(anterior, prime):
+        if (nt.isprime(i)):
+            count=count+1
+    return count
+
+if __name__ == "__main__":
+    file = open('.\merssene.txt')
+    lista = file.readline().split(",")
+    itemAnterior=1
+    for l in lista:
+
+        print(l + ": " + str(countPrimes(l,itemAnterior)))
+        itemAnterior=int(l)    
+    # # for i in range(1, 885046783):
+    #     print(i)
+
